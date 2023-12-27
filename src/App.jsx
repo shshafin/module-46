e1ddd12ad4fcde29633698025c3d13a1bef56c0d
shshafin <shafinsadnan08@gmail.com>
2023-12-27 20:58:25 +0600

    setUp

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+
+      <Form></Form>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,13 @@
+const Form = () => {
+  return (
+    <div>
+      <div>
+        <input type="text" />
+        <br />
+        <button >submit</button>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
